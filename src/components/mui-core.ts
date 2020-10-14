5a0625b8d109294939a1916c15fbec964afb7c8a
Roman Horokhov <nerveclasp@gmail.com>
2020-10-14 13:24:55 +0300

    maybe last react commit.. I think I want to have fun with this one and do some other project for recruiters

---
+++ src/components/mui-core.ts
@@ -0,0 +1,6 @@
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import Card from '@material-ui/core/Card';
+import Chip from '@material-ui/core/Chip';
+
+export { Button, Fab, Card, Chip };
